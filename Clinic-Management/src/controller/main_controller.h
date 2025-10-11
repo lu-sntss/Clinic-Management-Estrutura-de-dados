99750e6431b4efabe5e290e87251b78c7c11dbe5
TrMiguelTr <144959912+TrMiguelTr@users.noreply.github.com>
2025-10-11 17:11:02 -0300

    Delete Clinic-Management directory

--- Clinic-Management/src/controller/main_controller.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_CONTROLLER_H
-#define MAIN_CONTROLLER_H
-
-/* Mostra e controla o menu principal (loop). */
-void run_main_menu(void);
-
-#endif /* MAIN_CONTROLLER_H */
--- Clinic-Management/src/main.c
+++
@@ -1,7 +0,0 @@
-#include "controller/main_controller.h"
-
-int main(void) {
-    // Apenas executa o menu principal por enquanto.
-    run_main_menu();
-    return 0;
-}
